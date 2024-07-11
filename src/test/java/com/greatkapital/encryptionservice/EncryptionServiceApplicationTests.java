0336446c023ffc60255c3b5d94d286f4d93ac9e7
Neilay Bhatt <neilaybhatt27@gmail.com>
2024-07-11 14:48:12 +0530

    Project initialized

---
+++ src/test/java/com/greatkapital/encryptionservice/EncryptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatkapital.encryptionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
